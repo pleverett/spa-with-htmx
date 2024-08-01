d3405122c8ccfbbac3f7ebcec0bf0e46d05696da
Paul Leverett <pleverett@gmail.com>
2024-08-01 16:09:52 -0700

    Finished lesson 3

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Product Listing", articles: articles });
 });
 
+router.get("/articles/:id", (req, res) => {
+  const article = articles.find((a) => a.id === parseInt(req.params.id));
+
+  res.render("article", { title: article.name, article: article });
+});
+
 export default router;
